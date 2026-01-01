ccb2d9792041366ef2fbfaa5beb3c1a151403aca
Sebastian <88991140+stardustgd@users.noreply.github.com>
2025-12-31 20:08:10 -0800

    feat(web): implement basic ui (#9)

---
+++ web/src/app/screen/page.tsx
@@ -0,0 +1,17 @@
+import DateComponent from '@/components/screens/Date'
+import Message from '@/components/screens/Message'
+import Weather from '@/components/screens/Weather'
+
+export default function ScreenPage() {
+  return (
+    <div className="flex flex-col mx-8 my-6 gap-4 h-full">
+      <DateComponent />
+      <div className="border border-black">
+        <Weather />
+      </div>
+      <div className="border border-black">
+        <Message />
+      </div>
+    </div>
+  )
+}
---
+++ web/src/components/screens/Date.tsx
@@ -0,0 +1,26 @@
+export default function DateComponent() {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+
+  const date = new Date()
+
+  return (
+    <div>
+      <h1 className="font-semibold text-3xl">
+        {months[date.getMonth()]} {date.getDate()}, {date.getFullYear()}
+      </h1>
+    </div>
+  )
+}
