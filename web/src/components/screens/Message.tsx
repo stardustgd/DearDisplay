a6252144c2f798eef7cac0832df24a85e9fcc45b
stardust <88991140+stardustgd@users.noreply.github.com>
2025-12-31 20:07:20 -0800

    feat(web): implement basic ui

---
+++ web/src/components/screens/Message.tsx
@@ -0,0 +1,7 @@
+export default function Message() {
+  return (
+    <div className="flex justify-center m-4">
+      <h1>The message to be displayed on the screen goes here.</h1>
+    </div>
+  )
+}
