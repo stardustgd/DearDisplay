b0034983c1053d7a4fc5c9b9d4a375e987aab9d5
stardust <88991140+stardustgd@users.noreply.github.com>
2026-02-16 19:08:24 -0800

    fix(web): force dynamic rendering for screen page

--- web/src/app/screen/page.tsx
+++ web/src/app/screen/page.tsx
@@ -2,6 +2,8 @@ import DateComponent from '@/components/screens/Date'
 import Message from '@/components/screens/Message'
 import Weather from '@/components/screens/Weather'
 
+export const dynamic = 'force-dynamic'
+
 export default function ScreenPage() {
   return (
     <div className="flex flex-col mx-8 my-6 gap-4 h-full">
